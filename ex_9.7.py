b1977283012db9157849419c06a70dfb662d75b7
toxa <toxameosh@gmail.com>
2020-10-11 22:15:40 +0300

    11.10.2020

---
+++ ex_9.7.py
@@ -0,0 +1,41 @@
+class User:
+    def __init__(self, first_name, last_name, age, location):
+        self.first = first_name
+        self.last = last_name
+        self.age = age
+        self.location = location
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print("\nUser info:")
+        print(f"Name: {self.first} {self.last}")
+        print(f"Age: {self.age}")
+        print(f"Location: {self.location}")
+
+    def greet_user(self):
+        print(f"Hello, {self.first}!")
+
+    def increment_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, location):
+        super().__init__(self, first_name, last_name, age)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.location = location
+        self.privileges = None
+
+    def show_privileges(self, *privileges):
+        print(privileges)
+
+
+admin = Admin('aaa', 'ddd', 'fff', 'sss')
+admin.show_privileges('разрешено добавлять сообщения',
+                      'разрешено удалять пользователей',
+                      'разрешено банить пользователей',)
